6039873652ef00378cf5cb57f0929bb876c67070
Jonathon Turel <jturel@redhat.com>
2018-09-19 19:11:05 +0000

    WIP

--- webpack/scenes/RedHatRepositories/components/__tests__/RepositorySetRepositories.test.js
+++ webpack/scenes/RedHatRepositories/components/__tests__/RepositorySetRepositories.test.js
@@ -5,34 +5,35 @@ import toJson from 'enzyme-to-json';
 import configureMockStore from 'redux-mock-store';
 import RepositorySetRepositories from '../RepositorySetRepositories';
 
-
 const mockStore = configureMockStore([thunk]);
-const store = mockStore({data: {} });
+const store = mockStore({katello: { redHatRepositories: { repositorySetRepositories: [] } }});
 
 describe('RepositorySetRepositories Component', () => {
-  let shallowWrapper;
-  beforeEach(() => {
-    shallowWrapper = shallow(<RepositorySetRepositories
+  it('sorts repos correctly', async () => {
+    let shallowWrapper = shallow(<RepositorySetRepositories
       store={store}
       contentId={1}
-      productId={1}
+      productId={2}
       label="some label"
       type="foo"
     />);
-  });
 
-  afterEach(() => {
-    store.clearActions();
-  });
-
-  it('sorts repos correctly', async () => {
-    const sortedRepos = [
-      { arch: 'x86_64', releasever: '5.11' }
+    const repos = [
+      { arch: 'x86_64', releasever: '5.11' },
+      { arch: 'x86_64', releasever: '7Server' },
+      { arch: 'x86_64', releasever: '7.10' },
+      { arch: 'x86_64', releasever: '7.1' },
+      { arch: 'i386', releasever: '5.11' },
+      { arch: 'i386', releasever: '5Workstation' },
+      { arch: 'x86_64', releasever: '7.11' }
     ]
 
-    const result = shallowWrapper.instance().sortedRepos(sortedRepos);
-    sortedRepos.forEach((repo, i) => {
-      expect(result[i]).toEqual(sortedRepos[i]);
+    const result = shallowWrapper.dive().instance().sortedRepos(repos);
+
+    const expectedIndices = [1, 5, 6, 2, 3, 0, 4];
+
+    expectedIndices.forEach((expected, i) => {
+      expect(result[i]).toEqual(repos[expected]);
     });
   });
 });

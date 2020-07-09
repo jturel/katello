85ab94db3c0e180c5c9ffedbe4e138bf65e33433
Jonathon Turel <jturel@gmail.com>
2020-07-09 18:05:27 +0000

    fix associations and migrate provider url

--- db/migrate/20200703192121_create_cdn_configuration.katello.rb
+++ db/migrate/20200703192121_create_cdn_configuration.katello.rb
@@ -7,5 +7,16 @@ class CreateCdnConfiguration < ActiveRecord::Migration[6.0]
       t.integer :ssl_key_credential_id
       t.string :url
     end
+
+    add_foreign_key :katello_cdn_configurations, :taxonomies, name: 'katello_cdn_configurations_organization_id', column: :organization_id
+    add_foreign_key :katello_cdn_configurations, :katello_gpg_keys, name: 'katello_cdn_configurations_ssl_ca_credential_id', column: :ssl_ca_credential_id
+    add_foreign_key :katello_cdn_configurations, :katello_gpg_keys, name: 'katello_cdn_configurations_ssl_cert_credential_id', column: :ssl_cert_credential_id
+    add_foreign_key :katello_cdn_configurations, :katello_gpg_keys, name: 'katello_cdn_configurations_ssl_key_credential_id', column: :ssl_key_credential_id
+
+    ::Organization.all.each do |org|
+      org.create_cdn_configuration(url: org.redhat_provider.repository_url)
+    end
+
+    # we could drop repository_url from provider completely
   end
 end

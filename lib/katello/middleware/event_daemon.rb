0587a7d9eb3f17dc6d2a3b529a72a9e953781292
Jonathon Turel <jturel@gmail.com>
2021-02-12 16:18:46 +0000

    document daemon

--- lib/katello/middleware/event_daemon.rb
+++ lib/katello/middleware/event_daemon.rb
@@ -6,11 +6,17 @@ module Katello
       end
 
       def call(env)
-        Rails.logger.info(self.object_id)
+        # middleware is not necessarily thread safe because
+        # a single instance is created per process
+        # setting this instance variable would ordinarily not be advised
+        # but since Runner.start *is* thread safe, we can rely on that to
+        # ensure the daemon is only started once, and that we dont repeatedly
+        # hammer the Runner.start method which is relatively expensive
         unless @event_daemon_started
           Katello::EventDaemon::Runner.start
           @event_daemon_started = true
         end
+
         @app.call(env)
       end
     end
